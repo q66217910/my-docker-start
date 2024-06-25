e4fa81eef7eabd5f8469887fa26e389bcb4c99f7
de.zhang <de.zhang@swiftpass.cn>
2024-06-25 12:08:59 +0800

    自动初始化流水线

--- ci/jenkins/master/init.groovy.d/multibranch_pipeline.groovy
+++ ci/jenkins/master/init.groovy.d/multibranch_pipeline.groovy
@@ -9,17 +9,24 @@ import jenkins.branch.*
 import jenkins.plugins.git.*
 import org.jenkinsci.plugins.workflow.multibranch.*
 import com.cloudbees.hudson.plugins.folder.*
+import jenkins.plugins.git.traits.CleanBeforeCheckoutTrait
+import hudson.plugins.git.extensions.impl.CleanBeforeCheckout
+import com.cloudbees.hudson.plugins.folder.computed.DefaultOrphanedItemStrategy
 
-// Project variables
-List<String> gitRepos = ["https://github.com/Dirc/jenkins-init-groovy.git"]
+
+def initMultibranchPipelineStr = System.getenv('INIT_MULTIBRANCH_PIPELINE')
+List<String> gitRepos = initMultibranchPipelineStr?.split(',')
+
+println "==============新建流水线开始========================"
 
 gitRepos.forEach { gitRepo ->
-
-    String jobName = "${gitRepo}" as String
+    def repoName = gitRepo.substring(gitRepo.lastIndexOf('/') + 1, gitRepo.indexOf('.git'))
+    String jobName = "${repoName}" as String
     String jobDescription = "${gitRepo}" as String
     String credentialsId = "gitlab-builder"
     String jobScript = "Jenkinsfile"
 
+    println "==============新建流水线${jobName}========================"
 
     // Create MultiBranch pipeline
     Jenkins jenkins = Jenkins.instance
@@ -36,10 +43,20 @@ gitRepos.forEach { gitRepo ->
     GitSCMSource gitSCMSource = new GitSCMSource(id, remote, credentialsId, includes, excludes, ignoreOnPushNotifications)
     BranchSource branchSource = new BranchSource(gitSCMSource)
 
-    // Remove and replace?
+    //清理配置
+    CleanBeforeCheckout cleanCheckout = new CleanBeforeCheckout()
+    cleanCheckout.setDeleteUntrackedNestedRepositories(true)
+    CleanBeforeCheckoutTrait cleanBeforeCheckoutTrait = new CleanBeforeCheckoutTrait(cleanCheckout)
+
+    //设置配置
+    gitSCMSource.setTraits([cleanBeforeCheckoutTrait])
+
     PersistedList sources = mbp.getSourcesList()
     sources.clear()
     sources.add(branchSource)
+
+    //设置过期时间
+    mbp.setOrphanedItemStrategy(new DefaultOrphanedItemStrategy(true,30,10))
 
     // Trigger initial build (scan)
     jenkins.getItem(jobName).scheduleBuild()
