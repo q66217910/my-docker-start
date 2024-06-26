5699ba469255761da8ce6348ea7c14f54a3dfa46
de.zhang <de.zhang@swiftpass.cn>
2024-06-26 09:58:20 +0800

    oracel

--- ci/jenkins/master/init.groovy.d/multibranch_pipeline.groovy
+++ ci/jenkins/master/init.groovy.d/multibranch_pipeline.groovy
@@ -20,50 +20,55 @@ List<String> gitRepos = initMultibranchPipelineStr?.split(',')
 println "==============新建流水线开始========================"
 
 gitRepos.forEach { gitRepo ->
-    def repoName = gitRepo.substring(gitRepo.lastIndexOf('/') + 1, gitRepo.indexOf('.git'))
-    String jobName = "${repoName}" as String
-    String jobDescription = "${gitRepo}" as String
-    String credentialsId = "gitlab-builder"
-    String jobScript = "Jenkinsfile"
+    try{
+        def repoName = gitRepo.substring(gitRepo.lastIndexOf('/') + 1, gitRepo.indexOf('.git'))
+        String jobName = "${repoName}" as String
+        String jobDescription = "${gitRepo}" as String
+        String credentialsId = "gitlab-builder"
+        String jobScript = "Jenkinsfile"
 
-    println "==============新建流水线${jobName}========================"
+        println "==============新建流水线${jobName}========================"
 
-    // Create MultiBranch pipeline
-    Jenkins jenkins = Jenkins.instance
-    WorkflowMultiBranchProject mbp = jenkins.createProject(WorkflowMultiBranchProject.class, jobName)
-    mbp.description = jobDescription
-    mbp.getProjectFactory().setScriptPath(jobScript)
+        // Create MultiBranch pipeline
+        Jenkins jenkins = Jenkins.instance
+        WorkflowMultiBranchProject mbp = jenkins.createProject(WorkflowMultiBranchProject.class, jobName)
+        mbp.description = jobDescription
+        mbp.getProjectFactory().setScriptPath(jobScript)
 
-    // Add git repo
-    String id = "unneeded-non-null-ID"
-    String remote = gitRepo
-    String includes = "*"
-    String excludes = ""
-    boolean ignoreOnPushNotifications = false
-    GitSCMSource gitSCMSource = new GitSCMSource(id, remote, credentialsId, includes, excludes, ignoreOnPushNotifications)
-    BranchSource branchSource = new BranchSource(gitSCMSource)
+        // Add git repo
+        String id = "unneeded-non-null-ID"
+        String remote = gitRepo
+        String includes = "*"
+        String excludes = ""
+        boolean ignoreOnPushNotifications = false
+        GitSCMSource gitSCMSource = new GitSCMSource(id, remote, credentialsId, includes, excludes, ignoreOnPushNotifications)
+        BranchSource branchSource = new BranchSource(gitSCMSource)
 
-    //清理配置
-    CleanBeforeCheckout cleanCheckout = new CleanBeforeCheckout()
-    cleanCheckout.setDeleteUntrackedNestedRepositories(true)
-    CleanBeforeCheckoutTrait cleanBeforeCheckoutTrait = new CleanBeforeCheckoutTrait(cleanCheckout)
+        //清理配置
+        CleanBeforeCheckout cleanCheckout = new CleanBeforeCheckout()
+        cleanCheckout.setDeleteUntrackedNestedRepositories(true)
+        CleanBeforeCheckoutTrait cleanBeforeCheckoutTrait = new CleanBeforeCheckoutTrait(cleanCheckout)
 
-    //设置配置
-    gitSCMSource.setTraits([cleanBeforeCheckoutTrait])
+        //设置配置
+        gitSCMSource.setTraits([cleanBeforeCheckoutTrait])
 
-    PersistedList sources = mbp.getSourcesList()
-    sources.clear()
-    sources.add(branchSource)
+        PersistedList sources = mbp.getSourcesList()
+        sources.clear()
+        sources.add(branchSource)
 
-    //设置过期时间
-    mbp.setOrphanedItemStrategy(new DefaultOrphanedItemStrategy(true,30,10))
+        //设置过期时间
+        mbp.setOrphanedItemStrategy(new DefaultOrphanedItemStrategy(true,30,10))
 
-    // Trigger initial build (scan)
-    jenkins.getItem(jobName).scheduleBuild()
+        // Trigger initial build (scan)
+        jenkins.getItem(jobName).scheduleBuild()
 
-    jenkins.save()
+        jenkins.save()
 
-    println "==============新建流水线${jobName}成功========================"
+        println "==============新建流水线${jobName}成功========================"
+    }catch (Exception e){
+        println "==============新建流水线${jobName}失败========================"
+        println e.getMessage()
+    }
 
 }
 
