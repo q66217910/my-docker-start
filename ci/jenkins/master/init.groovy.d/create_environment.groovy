b74e593f093c07f07b628e5912bceaaf4ba33506
de.zhang <de.zhang@swiftpass.cn>
2024-07-08 11:24:13 +0800

    创建全局变量

--- ci/jenkins/master/init.groovy.d/create_environment.groovy
+++ ci/jenkins/master/init.groovy.d/create_environment.groovy
@@ -5,6 +5,9 @@ import hudson.slaves.*
 
 def instance = Jenkins.getInstance()
 
+def DOCKER_REGISTRY_URL = System.getenv('DOCKER_REGISTRY_URL')
+def DOCKER_REPOSITORY_URL = System.getenv('DOCKER_REPOSITORY_URL')
+
 println "==============创建环境变量========================"
 
 
@@ -18,6 +21,10 @@ if (envVarsProp == null) {
 
 // 添加新的环境变量到属性列表
 envVarsProp.getEnvVars().put("DEV_IN_BANK", "true")
+if (DOCKER_REGISTRY_URL != null && DOCKER_REPOSITORY_URL != null) {
+    envVarsProp.getEnvVars().put("DOCKER_PREFIX", DOCKER_REGISTRY_URL + "/" + DOCKER_REPOSITORY_URL)
+}
+
 
 properties.remove(envVarsProp.getClass())
 properties.add(envVarsProp)
